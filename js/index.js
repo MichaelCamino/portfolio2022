caa1c87ede465865636bfbcca75e67fd8b7064c9
Michael <77372266+MichaelCamino@users.noreply.github.com>
2022-03-31 17:11:13 -1000

    implements lottie files

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// WELCOME ANIMATION
+
+lottie.loadAnimation({
+    container: heroanimation, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'js/portfolio_hero_animation/data.json' // the path to the animation json
+});
