31d064661b6663d4c164140363102c2a8ead5a2b
Michael <77372266+MichaelCamino@users.noreply.github.com>
2022-04-12 13:25:51 -1000

    werks on js for werks subpages and subpage styling

--- js/script.js
+++ js/script.js
@@ -32,3 +32,22 @@ const myobserver = new IntersectionObserver(entries => {
 document.querySelectorAll('.animate-on-scroll').forEach((section) => {
     myobserver.observe(section);
 });
+
+// SLIDE RIDE
+
+const slideBox = document.querySelectorAll('.slide-box');
+for (let i = 0, len = slideBox.length; i < len; i++) {
+
+    let slideNavItems = slideBox[i].querySelectorAll('.slide-nav a');
+    for (let j = 0, len = slideNavItems.length; j < len; j++) {
+        console.log(slideNavItems[j]);
+        console.log(this);
+        slideNavItems[j].addEventListener('click', function (e) {
+            let currentSlideNum = "slide" + (j + 1);
+            slideBox[i].setAttribute('data-currentslide', currentSlideNum);
+            e.preventDefault();
+        });
+    };
+
+};
+
