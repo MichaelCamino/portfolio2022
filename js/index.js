61065b70a951bd8db4892fcfde3930c56a9fed75
Michael <77372266+MichaelCamino@users.noreply.github.com>
2022-04-20 19:14:48 -1000

    werks on ep page

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // WELCOME ANIMATION
 
 lottie.loadAnimation({
-    container: heroanimation, // the dom element that will contain the animation
+    container: square, // the dom element that will contain the animation
     renderer: 'svg',
     loop: true,
     autoplay: true,
-    path: 'js/portfolio_hero_animation/data.json' // the path to the animation json
+    path: 'js/square/data.json' // the path to the animation json
 });
