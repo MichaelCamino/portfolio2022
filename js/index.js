c6b41659551a54547cd8fb42a088944184365326
Michael <77372266+MichaelCamino@users.noreply.github.com>
2022-04-23 11:34:33 -1000

    werks on mainpage

--- js/index.js
+++ js/index.js
@@ -8,3 +8,12 @@ lottie.loadAnimation({
     path: 'js/portfolio_camera_animation/data.json' // the path to the animation json
 });
 
+// CAMERA ANIMATION
+
+lottie.loadAnimation({
+    container: scrolling, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'js/portfolio_scroll_animation/data.json' // the path to the animation json
+});
