78a4525d7dbb807644c79368174d0ed0982bc787
Michael <77372266+MichaelCamino@users.noreply.github.com>
2022-04-29 17:31:55 -1000

    werks on mobile styling

--- js/index.js
+++ js/index.js
@@ -17,3 +17,13 @@ lottie.loadAnimation({
     autoplay: true,
     path: 'js/portfolio_scroll_animation/data.json' // the path to the animation json
 });
+
+// COG ANIMATION
+
+lottie.loadAnimation({
+    container: cog, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'js/portfolio_cog_animation/data.json' // the path to the animation json
+});
