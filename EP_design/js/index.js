8264ff00cd8922a60d2909979f01b83a9267f16f
Michael <77372266+MichaelCamino@users.noreply.github.com>
2022-05-21 12:42:52 -1000

    updates aboot page animation

--- EP_design/js/index.js
+++
@@ -1,7 +0,0 @@
-lottie.loadAnimation({
-    container: gold, // the dom element that will contain the animation
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: '../js/portfolio_bis_ribbon_animation/data.json' // the path to the animation json
-});
---
+++ aboot/js/index.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+    container: aboot, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../js/portfolio_aboot_animation/data.json' // the path to the animation json
+});
