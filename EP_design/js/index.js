ea2da63be362745a743f406464f19c846b1886d0
Michael <77372266+MichaelCamino@users.noreply.github.com>
2022-05-21 14:03:27 -1000

    fixes ribbon animation

---
+++ EP_design/js/index.js
@@ -0,0 +1,8 @@
+lottie.loadAnimation({
+    container: bis, // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../js/portfolio_bis_ribbon_animation/data.json' // the path to the animation json
+});
+
